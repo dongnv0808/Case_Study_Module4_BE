375a68044da83cf6342150675774bf5e30a4a871
giabao2k <ngonguyengiabao2000@gmail.com>
2022-08-20 20:49:55 +0700

    bao

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -6,6 +6,8 @@ import { SECRET_KEY } from "../middleware/auth";
 
 class AuthController {
     register = async (req, res) => {
+        console.log(123);
+        
         let user = req.body;
         let checkUsername = await User.findOne({
             username: user.username
